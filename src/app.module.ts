060b6a2ed5efe115ecdcbabe0bd07466a92261d1
Bowie <cleiton-andrade@outlook.com>
2025-12-18 11:03:57 -0300

    Atualização main

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,21 +10,29 @@ import { AuthModule } from './auth/auth.module';
 import { UsuarioModule } from './usuario/usuario.module';
 import { RecomendacaoModule } from './recomendacao/recomendacao.module';
 import { AppController } from './app.controller';
+import { ProdService } from './data/services/prod.service';
+import { DevService } from './data/services/dev.service';
 
 @Module({
+  // imports: [
+  //   ConfigModule.forRoot({
+  //     isGlobal: true,
+  //   }),
+  //   TypeOrmModule.forRoot({
+  //     type: 'mysql',
+  //     host: process.env.DB_HOST,
+  //     port: 3306,
+  //     username: process.env.DB_USERNAME,
+  //     password: process.env.DB_PASSWORD,
+  //     database: process.env.DB_NAME,
+  //     entities: [Encomenda, Categoria, Usuario],
+  //     synchronize: true,
+  //   }),
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: 3306,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: [Encomenda, Categoria, Usuario],
-      synchronize: true,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useClass: DevService,
+      imports: [ConfigModule],
     }),
     EncomendaModule, CategoriaModule, AuthModule, UsuarioModule, RecomendacaoModule
   ],
--- src/data/services/dev.service.ts
+++ src/data/services/dev.service.ts
@@ -7,16 +7,16 @@ import { Encomenda } from "../../encomenda/entities/encomenda.entity";
 @Injectable()
 export class DevService implements TypeOrmOptionsFactory {
 
-    createTypeOrmOptions(): TypeOrmModuleOptions {
-        return {
-            type: 'mysql',
-            host: 'localhost',
-            port: 3306,
-            username: 'root@root.com',
-            password: 'rootroot',
-            database: 'db_nutrileve',
-            entities: [Categoria, Encomenda, Usuario],
-            synchronize: true,
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_nutrileve',
+      entities: [Categoria, Encomenda, Usuario],
+      synchronize: true,
     };
   }
 }
