1ea72459e03d2b3ea58be69639a7689c8cb39404
Beatriz Monteiro Vieira <beatrizmonteirovieira@outlook.com>
2026-02-02 10:34:29 -0300

    alteração em controller, service e auth de usuario

--- src/auth/service/auth.service.ts
+++ src/auth/service/auth.service.ts
@@ -1,59 +1,63 @@
 //importar as pastas para não dar erro (ctrl+espaço)
 
-import { JwtService } from "@nestjs/jwt"
-import { UsuarioService } from "../../usuario/services/usuario.service"
-import { Bcrypt } from "../bcrypt/bcrypt"
-import { HttpException, HttpStatus, Injectable } from "@nestjs/common"
-import { UsuarioLogin } from "../entities/usuariologin.entity"
+import { JwtService } from '@nestjs/jwt';
+import { UsuarioService } from '../../usuario/services/usuario.service';
+import { Bcrypt } from '../bcrypt/bcrypt';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { UsuarioLogin } from '../entities/usuariologin.entity';
 
 @Injectable()
 export class AuthService {
-    constructor(
-        private usuarioService: UsuarioService,
-        private jwtService: JwtService,
-        private bcrypt: Bcrypt
-    ) { }
+  constructor(
+    private usuarioService: UsuarioService,
+    private jwtService: JwtService,
+    private bcrypt: Bcrypt,
+  ) {}
 
-    // Método usado para validar o usuário
-    async validateUser(username: string, password: string): Promise<any> {
+  // Método usado para validar o usuário
+  async validateUser(username: string, password: string): Promise<any> {
+    const buscaUsuario = await this.usuarioService.findByUsuario(username);
 
-        const buscaUsuario = await this.usuarioService.findByUsuario(username)
+    if (!buscaUsuario)
+      throw new HttpException('Usuário não encontrado!', HttpStatus.NOT_FOUND);
 
-        if (!buscaUsuario)
-            throw new HttpException('Usuário não encontrado!', HttpStatus.NOT_FOUND)
+    // Faz a comparação entre a senha criptografada no banco com a que está vindo pela requisição
+    const matchPassword = await this.bcrypt.compararSenhas(
+      buscaUsuario.senha,
+      password,
+    );
 
-        // Faz a comparação entre a senha criptografada no banco com a que está vindo pela requisição
-        const matchPassword = await this.bcrypt.compararSenhas(buscaUsuario.senha, password)
-
-        // Se as info estiverem corretas, entra no IF
-        if (buscaUsuario && matchPassword) {
-            const { senha, ...resposta } = buscaUsuario // Desestruturamos o objeto, isto é, pegamos o que importa para nós
-            return resposta
-        }
-
-        return null
+    // Se as info estiverem corretas, entra no IF
+    if (buscaUsuario && matchPassword) {
+      const { senha, ...resposta } = buscaUsuario; // Desestruturamos o objeto, isto é, pegamos o que importa para nós
+      return resposta;
     }
 
-    async login(usuarioLogin: UsuarioLogin) {
+    return null;
+  }
 
-        const usuarioValidado = await this.validateUser(
-            usuarioLogin.usuario,
-            usuarioLogin.senha
-        )
+  async login(usuarioLogin: UsuarioLogin) {
+    const usuarioValidado = await this.validateUser(
+      usuarioLogin.usuario,
+      usuarioLogin.senha,
+    );
 
-        if (!usuarioValidado)
-            throw new HttpException('Senha inválida!', HttpStatus.UNAUTHORIZED)
+    if (!usuarioValidado)
+      throw new HttpException('Senha inválida!', HttpStatus.UNAUTHORIZED);
 
-        const payload = { sub: usuarioLogin.usuario }
+    const payload = {
+      id: usuarioValidado.id,
+      usuario: usuarioValidado.usuario,
+      tipo_usuario: usuarioValidado.tipo_usuario,
+    };
 
-        return {
-            id: usuarioValidado.id,
-            nome: usuarioValidado.nome,
-            usuario: usuarioLogin.usuario,
-            senha: '',
-            foto: usuarioValidado.foto,
-            token: `Bearer ${this.jwtService.sign(payload)}`,
-        }
-    }
-
+    return {
+      id: usuarioValidado.id,
+      nome: usuarioValidado.nome,
+      usuario: usuarioLogin.usuario,
+      senha: '',
+      foto: usuarioValidado.foto,
+      token: `Bearer ${this.jwtService.sign(payload)}`,
+    };
+  }
 }
